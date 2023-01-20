ba8a9c960845ad4c70f1ac0e0f60b543b8a4ce59
Marc Smith <theonlymarcsmith@gmail.com>
2023-01-19 22:05:15 -0500

    Addressed code review recommendations

--- main.py
+++ main.py
@@ -2,18 +2,15 @@ import sys
 test_verb = "aller";
 
 french_test_list = ["je vais","tu vas", 'il va', "elle va", "on va","nous allons", "vous allez", "ils vont", "elles vont"]
-english_test_list = ["I go", "You go", "He goes", "She goes", "One goes", "We go", "You go", "They go (masc)", "They Go (fem)"]
+english_test_list = ["I go", "You go", "He goes", "She goes", "One goes", "We go", "You go", "They go (masc)", "They go (fem)"]
 
 def get_user_input():
-    'Prompt user to enter an answer and sanitize it'
-    print("--> ", end="")
-    answer = input()
-    # answer = answer.strip()
-    # answer = answer.replace(" ","").lower()
+    'Prompt user to enter an answer'
+    # print("--> ", end="")
+    answer = input("--> ", end="")    
     return answer
 
-def sanitize_data(verb):
-    'Remove whitespaces'
+def sanitize_data(verb):   
     verb = verb.strip().replace(" ","").lower()
     return verb
 
@@ -45,17 +42,17 @@ def display_menu():
     print("2 - Drill and Practice")
     print("3 - Exit")
     print("--> ", end="")
-    selection = int(input())
+    selection = input()
     return selection
 
 def main():
     while True:
         selection = display_menu()
-        if selection == 1:
+        if selection == '1':
             display_full_verb_conjugation()
-        elif selection == 2:
+        elif selection == '2':
             display_question()
-        elif selection == 3:
+        elif selection == '3':
             sys.exit("Goodbye")
         else:
             print(" Option not recognised, try again")
